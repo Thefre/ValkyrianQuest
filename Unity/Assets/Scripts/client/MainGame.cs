9c3212eb885802bbd618c6cfa509c7cf4b1945cf
Thefre <thefre@gmail.com>
2014-02-01 19:44:25 -0600

    Started dev console implementation.

--- Unity/Assets/Scripts/client/MainGame.cs
+++ Unity/Assets/Scripts/client/MainGame.cs
@@ -7,15 +7,20 @@ public class MainGame : MonoBehaviour {
 
 	public SpriteData[] spriteData;
 
+	private Vector2 scrollPosition;
+	public string message = "Console started.";
+
 	// Use this for initialization
 	void Start () {
 		Application.targetFrameRate = -1;
 
 		// Assign Menus
 		Window newWindow = new Window();
-		newWindow.Initialize("main", 0, 0, 120, 100, MainMenu, Window.Align.Center, "Main Menu");
+		newWindow.Initialize("main", 0, 0, 120, 120, MainMenu, Window.Align.Center, "Main Menu");
 		GUIManager.windows.Add(newWindow);
-
+		newWindow = new Window();
+		newWindow.Initialize("devConsole", 0, 0, 250, 120, DevConsole, Window.Align.BottomRight);
+		GUIManager.windows.Add(newWindow);
 	}
 	
 	// Update is called once per frame
@@ -25,6 +30,9 @@ public class MainGame : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.Space)) {
 			GUIManager.state = "main";
 		}
+		if(Input.GetKeyDown(KeyCode.A)) {
+			message += "\nTest";
+		}
 	}
 
 	public void MainMenu() {
@@ -32,7 +40,13 @@ public class MainGame : MonoBehaviour {
 			GUIManager.state = "networkMenu";
 		if(GUILayout.Button("Anim Test"))
 			GUIManager.state = "animTest";
-
+		if(GUILayout.Button("Console"))
+			GUIManager.state = "devConsole";
 	}
 
+	public void DevConsole() {
+		scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(240), GUILayout.Height(110));
+		GUILayout.Label(message);
+		GUILayout.EndScrollView();
+	}
 }
