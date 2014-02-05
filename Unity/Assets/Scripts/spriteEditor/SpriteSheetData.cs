56a06355df43fbd9398e2ea6b83dbfba5e4838aa
Thefre <thefre@gmail.com>
2014-02-05 06:09:20 -0600

    Started on spritesheet data.
    The sprite sheet data will hold the palettes for a sprite sheet, as well
    as the base and colored textures. There will be a function for drawing
    onto the texture eventually.

---
+++ Unity/Assets/Scripts/spriteEditor/SpriteSheetData.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteSheetData : MonoBehaviour {
+	public int tileSize;
+	public int sheetWidth;
+	public int sheetHeight;
+	public int palette;
+
+	public Texture2D baseSheet;
+	public Texture2D colorPalettes;
+	public Texture2D coloredSheet;
+
+	public void Initialize() {
+		ResetPalettes();
+	}
+
+	public void NewSheets(int tile, int width, int height) {
+		baseSheet = new Texture2D(tile*width, tile*height,TextureFormat.ARGB32,false);
+		coloredSheet = new Texture2D(tile*width, tile*height,TextureFormat.ARGB32,false);
+		for (int x = 0; x < 32; ++x) {
+			for (int y = 0; y < 32; ++y) {
+				baseSheet.SetPixel(x,y,new Color(0,0,0,0));
+				coloredSheet.SetPixel(x,y,new Color(0,0,0,0));
+			}
+		}
+	}
+
+	public void ResetPalettes() {
+		colorPalettes = new Texture2D(32,32);
+		for (int x = 0; x < 32; x++) {
+			for (int y = 0; y < 32; y++) {
+				if(y==0) {
+					colorPalettes.SetPixel(x+1,y+1,new Color((float)(x/255),(float)(x/255),(float)(x/255),1));
+				} else {
+					colorPalettes.SetPixel(x+1,y+1,new Color((float)((x*7)/255),(float)((x*7)/255),(float)((x*7)/255),1));
+				}
+			}
+		}
+	}
+}
